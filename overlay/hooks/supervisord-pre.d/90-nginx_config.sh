105ac2fe2a1a8402f1121e06ba09d5c24150139c
Josh.5 <jsunnex@gmail.com>
2020-02-28 14:05:07 +1300

    There seems to be an issue with permissions of the nginx log files. This change ensure that the log files have the correct permissions when the container starts.

--- overlay/hooks/supervisord-pre.d/90-nginx_config.sh
+++ overlay/hooks/supervisord-pre.d/90-nginx_config.sh
@@ -10,19 +10,33 @@ sed -i -e "s|access_log .*$|error_log /var/log/nginx/access.log;|"  /etc/nginx/s
 sed -i -e "s|error_log .*$|error_log /var/log/nginx/error.log;|"  /etc/nginx/sites-available/10_generic.conf;
 
 
+# Ensure nginx log files are not symlinks
+if [[ -h /var/log/nginx/access.log ]]; then
+    rm -f /var/log/nginx/access.log;
+    touch /var/log/nginx/access.log;
+fi
+if [[ -h /var/log/nginx/error.log ]]; then
+    rm -f /var/log/nginx/error.log;
+    touch /var/log/nginx/error.log;
+fi
+
+# Set permissions of nginx log files
+chown -R ${WEBUSER}:adm /var/log/nginx/*
+
+
 # TODO: Make this work at some point... I think it is nicer to point logs to stdout 
 # Set default access logs to stdout and stderr
 # if [ "${LOG_TO_FILES}" = "false" ]; then
 # 
 #     # Set nginx access log to stdout
 #     if [[ ! -h /var/log/nginx/access.log ]]; then
-#         rm -rf /var/log/nginx/access.log;
+#         rm -f /var/log/nginx/access.log;
 #         ln -s /dev/stdout /var/log/nginx/access.log;
 #     fi
 # 
 #     # Set nginx access log to stderr
 #     if [[ ! -h /var/log/nginx/error.log ]]; then
-#         rm -rf /var/log/nginx/error.log;
+#         rm -f /var/log/nginx/error.log;
 #         ln -s /dev/stderr /var/log/nginx/error.log;
 #     fi
 # 
